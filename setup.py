881ff500ecdba8cc01caf8f427802d1f4771720b
alex <alex@eat-up.org>
2013-01-25 21:36:49 -0800

    requirements.txt, config.conf

--- setup.py
+++ setup.py
@@ -11,15 +11,16 @@ def find_package_data(package, files=()):
                 package_data.setdefault(package_path, []).append(f)
     return package_data
 
+with open('requirements.txt') as f:
+    requirements = map(str.strip, f)
 
 setup(name='loggingcontext',
-      description='Logging Magic',
+      description='Magic Automatic Logging Context',
       version=os.environ.get('GIT_BRANCH'),
       author='Alex',
       author_email='alex@eat-up.org',
       packages=find_packages(),
-      install_requires=[
-        'pika',
-        'nose',
-      ]
+      install_requires=requirements,
+      package_data=find_package_data('loggingcontext',
+                                     files=('config.conf',))
 )
